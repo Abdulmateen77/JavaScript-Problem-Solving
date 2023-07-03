b0a4ae6af0cc05df1276913b1d05fe463c4402c4
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-03 12:35:57 +0100

    Create GetName-by-Map.js

---
+++ Arrays/GetName-by-Map.js
@@ -0,0 +1,19 @@
+function getFullName(arr) {
+  // Using the map method to iterate over the array of objects and concatenate firstname and lastname
+  const fullName = arr.map((person) => person.firstname + ' ' + person.lastname);
+  return fullName;
+}
+
+if (
+  getFullName([
+    { firstname: "Robert", lastname: "Obrain" },
+    { firstname: "Kevin", lastname: "Hart" },
+    { firstname: "Dane", lastname: "Cobby" }
+  ]).toString() != ["Robert Obrain", "Kevin Hart", "Dane Cobby"]
+) {
+  console.log("Test fails: Expected output: ['Robert Obrain', 'Kevin Hart', 'Dane Cobby']");
+} else {
+  console.log("Sample test case passed!");
+}
+
+module.exports = getFullName;
