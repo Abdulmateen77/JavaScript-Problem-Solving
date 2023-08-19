b2978b495bc34d9805b7349e935541c418a5ca5f
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-19 08:42:23 +0100

    Update Calculate -number-of-days.js

--- Calculate -number-of-days.js
+++ Calculate -number-of-days.js
@@ -1,19 +1,27 @@
-// Problem Description
-// You are given a date in the form of "DD/MM/YYYY". Calculate the number of days between the given date and today's date.
+// Problem Description:
+// You are given a date in the form of "DD/MM/YYYY".
+// Calculate the number of days between the given date and today's date.
 // Today's day is the date when the program is executed.
 
-
+// Define a function to calculate the number of days between a given date and today's date
 function getNumberOfDays(date) {
-      const todaysDateTime = new Date().setUTCHours(0,0,0,0)
+    // Get the current date and time, and set the time to midnight (00:00:00)
+    const todaysDateTime = new Date().setUTCHours(0, 0, 0, 0);
 
-      const arr = date.split("/")
-      date =  arr[1] + "/" + arr[0] + "/" + arr[2]
+    // Split the given date into day, month, and year components
+    const arr = date.split("/");
+    // Reformat the date string to match "MM/DD/YYYY" format
+    date = arr[1] + "/" + arr[0] + "/" + arr[2];
 
-      const givenDateTime = new Date(date).setUTCHours(0,0,0,0)
+    // Get the date and time of the given date, and set the time to midnight (00:00:00)
+    const givenDateTime = new Date(date).setUTCHours(0, 0, 0, 0);
 
-      const timeDifference = todaysDateTime - givenDateTime
+    // Calculate the time difference between today's date and the given date in milliseconds
+    const timeDifference = todaysDateTime - givenDateTime;
 
-      const days = timeDifference/(1000*60*60*24)
+    // Convert the time difference to days and return the result
+    const days = timeDifference / (1000 * 60 * 60 * 24);
 
-      return Math.floor(days);
+    // Return the number of days (rounded down to the nearest whole number)
+    return Math.floor(days);
 }
