221787c37978819df9cf5be375d9a58d78d10824
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-20 09:40:27 +0100

    Update arrow-map-func.js

--- Arrays/arrow-map-func.js
+++ Arrays/arrow-map-func.js
@@ -1,31 +1,29 @@
+// Define a function 'modify' that takes an array as input and performs specific operations on each element
 let modify = (array) => {
+    // Create a new array using the 'map' function
     let newArray = array.map(item => {
-          if (isPrime(item)){
-                return item + 1;
-          } else{
-                return item * 2;
-          }
-    })
-    return newArray;
+        // Check if the current item is prime using the 'isPrime' function
+        if (isPrime(item)) {
+            return item + 1; // Increment the item by 1 if it's prime
+        } else {
+            return item * 2; // Multiply the item by 2 if it's not prime
+        }
+    });
+    return newArray; // Return the new array
 }
 
-
+// Define a function 'isPrime' to check if a number is prime
 function isPrime(n) {
-
-if(n == 0 || n == 1) {
-
-    return false;
-
-}
-
-for(let i = 2 ; i*i <= n ; i++) {
-
-    if(n%i == 0) {
-
-        return false;
-
+    if (n == 0 || n == 1) {
+        return false; // 0 and 1 are not prime
     }
 
-}
+    // Check divisibility up to the square root of 'n'
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false; // If 'n' is divisible by any number, it's not prime
+        }
+    }
 
-return true;}
+    return true; // 'n' is prime if not divisible by any number other than 1 and itself
+}
