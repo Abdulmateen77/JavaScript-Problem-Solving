86eb5a823b7001141796e441f70c6d8d4bd25c47
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-24 10:58:56 +0100

    Update Higher-order-functions-Maps.js

--- Arrays/Higher-order-functions-Maps.js
+++ Arrays/Higher-order-functions-Maps.js
@@ -1,10 +1,14 @@
+// Define a function called findLength that takes an array 'arr' as input
 function findLength(arr) {
+    // Use the map() function to transform each element 'str' in the array 'arr'
     arr = arr.map((str) => {
-          if (str === null || str === undefined){
-                return 0;
-          } else {
-                return str.length;
-          }
-    })
+        // Check if the current element 'str' is null or undefined
+        if (str === null || str === undefined) {
+            return 0;  // If it is null or undefined, return 0
+        } else {
+            return str.length;  // If not null or undefined, return the length of the string 'str'
+        }
+    });
+    // Return the modified array 'arr' where each element has been replaced with its length
     return arr;
 }
