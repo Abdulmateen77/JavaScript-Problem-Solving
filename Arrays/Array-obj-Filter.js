2fb9fa86f2b3ff431fd638db65090cec719724ad
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-24 11:01:21 +0100

    Update Array-obj-Filter.js

--- Arrays/Array-obj-Filter.js
+++ Arrays/Array-obj-Filter.js
@@ -1,13 +1,18 @@
+// Function to filter an array of objects based on the city property
 function filterByCity(arr) {
-      const resultarr = arr.filter((item) => {
-            let {city} = item
-            city = city.toLowerCase()
-            if (city === "bangalore" || city === "hyderabad"){
-                  return true;
-            } else {
-                  return false;
-            }
-      })
+    // Create an array to store the filtered results
+    const resultArr = arr.filter((item) => {
+        // Extract the city property and convert it to lowercase
+        let { city } = item;
+        city = city.toLowerCase();
+        
+        // Check if the city is "bangalore" or "hyderabad"
+        if (city === "bangalore" || city === "hyderabad") {
+            return true; // Include the item in the filtered array
+        } else {
+            return false; // Exclude the item from the filtered array
+        }
+    });
 
-      return resultarr;
+    return resultArr; // Return the filtered array
 }
