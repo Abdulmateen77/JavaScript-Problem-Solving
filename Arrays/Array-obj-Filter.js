da76eeefdb9a3accc1fc4cce3727412ee0fb9405
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-24 11:02:04 +0100

    Update Array-obj-Filter.js

--- Arrays/Array-obj-Filter.js
+++ Arrays/Array-obj-Filter.js
@@ -16,3 +16,36 @@ function filterByCity(arr) {
 
     return resultArr; // Return the filtered array
 }
+
+// Test Case 1: Filtering out items with city other than "bangalore" and "hyderabad"
+const input1 = [
+    { city: "bangalore", name: "John" },
+    { city: "chennai", name: "Alice" },
+    { city: "hyderabad", name: "Bob" },
+    { city: "mumbai", name: "Eve" }
+];
+console.log(filterByCity(input1)); 
+// Output: [ { city: 'bangalore', name: 'John' }, { city: 'hyderabad', name: 'Bob' } ]
+
+// Test Case 2: All items have city as "bangalore" or "hyderabad"
+const input2 = [
+    { city: "bangalore", name: "Alex" },
+    { city: "bangalore", name: "Emma" },
+    { city: "hyderabad", name: "Daniel" }
+];
+console.log(filterByCity(input2)); 
+// Output: [ { city: 'bangalore', name: 'Alex' }, { city: 'bangalore', name: 'Emma' }, { city: 'hyderabad', name: 'Daniel' } ]
+
+// Test Case 3: Empty input array
+const input3 = [];
+console.log(filterByCity(input3)); 
+// Output: []
+
+// Test Case 4: Filtering out items with mixed-case cities
+const input4 = [
+    { city: "Bangalore", name: "Sophia" },
+    { city: "Hyderabad", name: "Liam" },
+    { city: "CHENNAI", name: "Olivia" }
+];
+console.log(filterByCity(input4)); 
+// Output: [ { city: 'Bangalore', name: 'Sophia' }, { city: 'Hyderabad', name: 'Liam' } ]
