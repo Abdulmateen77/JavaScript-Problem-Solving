1a7895d41f769f245c6f75bc3565047852456672
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-25 22:19:36 +0100

    Update try-catch.js

--- try-catch.js
+++ try-catch.js
@@ -1,18 +1,19 @@
+// Function to handle errors
 function errorHandler(x) {
-    try{
+    try {
+        // Try to get the value using the getX function
         let num = getX(x);
-        return num;
-    } catch (error){
-        console.error("An error occurred:", error.message);
-        return null;
-
+        return num; // Return the value if successful
+    } catch (error) {
+        console.error("An error occurred:", error.message); // Log the error message
+        return null; // Return null to indicate error
     }
-
 }
 
+// Function to get the value of x
 function getX(x) {
-    if(x == 5) {
-        return new Error("Error Occured");
+    if (x == 5) {
+        return new Error("Error Occurred"); // Return an Error object with a message if x is 5
     }
-    return x;
+    return x; // Return x if x is not 5
 }
