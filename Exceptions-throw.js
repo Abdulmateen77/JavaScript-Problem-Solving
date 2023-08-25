e281ab2728227b2adddad5edd4c02061124e7197
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-25 22:22:59 +0100

    Update Exceptions-throw.js

--- Exceptions-throw.js
+++ Exceptions-throw.js
@@ -1,37 +1,26 @@
+// Function to determine if the given input is an alphabet character
 function isAlphabet(X) {
-      if (checkAlphabet(X)){
-            return "Yes";
-      } else {
-            throw "Not Alphabet";
-      }
-};
+    if (checkAlphabet(X)) {
+        return "Yes"; // Return "Yes" if X is an alphabet character
+    } else {
+        throw "Not Alphabet"; // Throw an error if X is not an alphabet character
+    }
+}
 
+// Function to check if a given character is an alphabet
 function checkAlphabet(X) {
+    // Get the ASCII code of the character
+    let n = X.charCodeAt(0);
 
-	let n = X.charCodeAt(0);
+    // Determine if the character's ASCII code falls within the range of uppercase or lowercase letters
+    let strStartsWithALetter = (n >= 65 && n < 91) || (n >= 97 && n < 123);
 
-	let strStartsWithALetter = (n>=65 && n < 91) || ( n>=97 && n<123);
-
-	
-
-// You can Google about what is ASCII code to know more about
-
-	// why are we using numbers like 65, 91, 97 & 123
-
-	
-
-if (strStartsWithALetter) {
-
-	// Yes, it starts with an alphabet
-
-	return true;
-
-} else {
-
-	// No, it doesnt start with an alphabet
-
-	return false;
-
-} 
-
+    // Check if the character starts with an alphabet
+    if (strStartsWithALetter) {
+        // Yes, it starts with an alphabet
+        return true;
+    } else {
+        // No, it doesn't start with an alphabet
+        return false;
+    }
 }
