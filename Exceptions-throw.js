831ecd12b443cb3f0ccb5ec5e57a00c84e73f446
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-26 10:14:08 +0100

    Update Exceptions-throw.js

--- Exceptions-throw.js
+++ Exceptions-throw.js
@@ -33,3 +33,9 @@ console.log(isAlphabet('5')); // Output: Not Alphabet (non-alphabet character)
 console.log(isAlphabet('@')); // Output: Not Alphabet (non-alphabet character)
 console.log(isAlphabet(''));  // Output: Not Alphabet (empty string)
 
+// Test cases for checkAlphabet function
+console.log(checkAlphabet('A')); // Output: true (uppercase alphabet)
+console.log(checkAlphabet('z')); // Output: true (lowercase alphabet)
+console.log(checkAlphabet('5')); // Output: false (non-alphabet character)
+console.log(checkAlphabet('@')); // Output: false (non-alphabet character)
+console.log(checkAlphabet(''));  // Output: false (empty string)
