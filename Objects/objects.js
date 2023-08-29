817be7643c4c8f8062e2596cdbd1d185c5fe35d7
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-29 23:25:37 +0100

    Update objects.js

--- Objects/objects.js
+++ Objects/objects.js
@@ -62,15 +62,3 @@ let blogPost = {
 console.log(blogPost)
 
 
-// constructor function to create blog posts
-function Post (title,body,author){
-  this.title = title,
-    this.body = body,
-    this.author = author,
-    this.views = 0,
-    this.comments = [],
-    this.isLive = false
-}
-
-let Blog = new Post("A","B", "C");
-console.log(Blog)
