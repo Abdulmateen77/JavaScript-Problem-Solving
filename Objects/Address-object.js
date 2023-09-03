a36e1dbe3dc57cb7f4b8e473f97352954df1c38d
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-03 22:48:37 +0100

    Update Address-object.js

--- Objects/Address-object.js
+++ Objects/Address-object.js
@@ -1,19 +1,24 @@
-// Address object
+// Address object representing a physical address
 let address = {
-  street : "Oxford street",
-  city : "London",
+  street: "Oxford street",
+  city: "London",
   zipCode: "W1C 1DL",
 }
-// Function to show address
-function showAddress (address){
-  for (key in address)
-    console.log(key, ":", address[key]);
+
+// Function to display the properties of an address object
+function showAddress(address) {
+  // Loop through the properties of the address object
+  for (key in address) {
+    console.log(key, ":", address[key]); // Output the property and its value
+  }
 }
 
-console.log(showAddress(address))
+// Display the address using the showAddress function
+console.log(showAddress(address));
 
-// Factory Function to create objects
-function createAddress (street, city, zipCode){
+// Factory Function to create address objects
+function createAddress(street, city, zipCode) {
+  // Return a new address object with the specified properties
   return {
     street,
     city,
@@ -21,20 +26,23 @@ function createAddress (street, city, zipCode){
   }
 }
 
-let addres = createAddress("oxford street","london", "W1C 1DL");
-console.log(addres)
+// Create a new address object using the factory function
+let newAddress = createAddress("Oxford Street", "London", "W1C 1DL");
+console.log(newAddress);
 
-
-// function to check properties of two object
-function areEqual(address1, address2){
+// Function to check if two address objects have the same properties
+function areEqual(address1, address2) {
   return address1.street === address2.street &&
     address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-    
+    address1.zipCode === address2.zipCode;
 }
-// function to check if the objects are same
-function areSame (address1, address2){
-   return address1 === address2;
+
+// Function to check if two address objects reference the exact same object in memory
+function areSame(address1, address2) {
+  return address1 === address2;
 }
-console.log(areEqual(newAddress, newAddress2))
-console.log(areSame(newAddress, newAddress2))
+
+// Example usage of the areEqual and areSame functions
+let newAddress2 = createAddress("Oxford Street", "London", "W1C 1DL");
+console.log(areEqual(newAddress, newAddress2)); // Check if properties are equal
+console.log(areSame(newAddress, newAddress2));  // Check if references are the same
