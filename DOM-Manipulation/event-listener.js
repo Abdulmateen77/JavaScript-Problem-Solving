5ddcd0fa5c502ed5cf3a8b2f7e7520baf0aa452f
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-03 22:50:14 +0100

    Update event-listener.js

--- DOM-Manipulation/event-listener.js
+++ DOM-Manipulation/event-listener.js
@@ -1,35 +1,42 @@
-const button = document.createElement("button")
+// Create a button element and set its inner HTML
+const button = document.createElement("button");
+button.innerHTML = `Show Table`;
 
-button.innerHTML = ` Show Table`
-document.body.append(button)
+// Append the button to the document body
+document.body.append(button);
 
-const showButton = document.querySelector("button")
+// Select the button element
+const showButton = document.querySelector("button");
 
-showButton.onclick = function(){
-    loadTableFromJson();
+// Add a click event listener to the button
+showButton.onclick = function () {
+  // Call the loadTableFromJson function when the button is clicked
+  loadTableFromJson();
 };
 
-const inputText = document.createElement("inputText")
+// Create an inputText element
+const inputText = document.createElement("inputText");
 
+// Set the inner HTML of the inputText element, including an input field and a display div
 inputText.innerHTML = `
 <input type="text" name="name" id="name" placeholder="Enter your name">
-
 <div id="name-display"></div>
-`
+`;
 
+// Append the inputText element to the document body
 document.body.append(inputText);
 
-
+// Select the input element
 let nameInput = document.querySelector("input");
 
-
+// Add an input event listener to the input element
 nameInput.addEventListener("input", function (event) {
-
+  // Get the input text from the event
   let inputText = event.target.value;
 
-
+  // Select the name-display div
   let nameDisplayDiv = document.querySelector("#name-display");
 
+  // Set the text content of the name-display div to the input text
   nameDisplayDiv.innerText = inputText;
-
 });
