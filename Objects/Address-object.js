33afc6288f64f163e8ee555aec77ce0efaff5051
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-03 22:47:54 +0100

    Create  Address-object.js

---
+++ Objects/Address-object.js
@@ -0,0 +1,40 @@
+// Address object
+let address = {
+  street : "Oxford street",
+  city : "London",
+  zipCode: "W1C 1DL",
+}
+// Function to show address
+function showAddress (address){
+  for (key in address)
+    console.log(key, ":", address[key]);
+}
+
+console.log(showAddress(address))
+
+// Factory Function to create objects
+function createAddress (street, city, zipCode){
+  return {
+    street,
+    city,
+    zipCode
+  }
+}
+
+let addres = createAddress("oxford street","london", "W1C 1DL");
+console.log(addres)
+
+
+// function to check properties of two object
+function areEqual(address1, address2){
+  return address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+    
+}
+// function to check if the objects are same
+function areSame (address1, address2){
+   return address1 === address2;
+}
+console.log(areEqual(newAddress, newAddress2))
+console.log(areSame(newAddress, newAddress2))
