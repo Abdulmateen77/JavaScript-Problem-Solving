34867ee848918b8e160414aa48f5bc0560671332
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-05 23:30:13 +0100

    Update calculate-grades.js

--- Arrays/calculate-grades.js
+++ Arrays/calculate-grades.js
@@ -29,3 +29,28 @@ const mar = [99, 80, 80];
 
 // Calculate and print the grade for the example marks
 console.log(calculateGrade(mar));
+
+
+// Test case 1: Average marks that result in an 'A' grade
+const test1Marks = [90, 92, 95];
+const test1ExpectedGrade = 'A';
+const test1Result = calculateGrade(test1Marks);
+
+console.log('Test Case 1:');
+console.log(`Input Marks: ${test1Marks}`);
+console.log(`Expected Grade: ${test1ExpectedGrade}`);
+console.log(`Actual Grade: ${test1Result}`);
+console.log(`Test Passed: ${test1ExpectedGrade === test1Result}`);
+console.log('-------------------------------------');
+
+// Test case 2: Average marks that result in a 'C' grade
+const test2Marks = [70, 75, 78];
+const test2ExpectedGrade = 'C';
+const test2Result = calculateGrade(test2Marks);
+
+console.log('Test Case 2:');
+console.log(`Input Marks: ${test2Marks}`);
+console.log(`Expected Grade: ${test2ExpectedGrade}`);
+console.log(`Actual Grade: ${test2Result}`);
+console.log(`Test Passed: ${test2ExpectedGrade === test2Result}`);
+console.log('-------------------------------------');
