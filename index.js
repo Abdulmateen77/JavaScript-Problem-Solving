74b0a3f71e7e8f416cfb45a6653ac26b78edb372
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-05 23:28:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,29 +51,6 @@ function sum(limit){
 
 console.log(sum(10))
 
-// calculate grades
-function calculateGrade(marks){
-  const avg = calculateAverage(marks)
-
-  if (avg <= 59) return "F";
-  else if (avg <= 69) return "D";
-  else if (avg <= 79) return "C";
-  else if (avg <= 89) return "B";
-  else if (avg <= 100) return "A";
-  
-}
-
-// calculate average
-function calculateAverage(array){
-  let sum = 0;
-  
-  for (let value of array)
-    sum += value;
-  return sum / array.length; 
-}
-
-const mar = [99,80,80];
-console.log(calculateGrade(mar));
 
 //show prime numbers function
 function showPrime(num){
