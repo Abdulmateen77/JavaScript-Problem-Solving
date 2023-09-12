8a5ba8e06a1da2f09523a1fc73ef31da834c241a
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-12 23:56:19 +0100

    Update array.js

--- Arrays/array.js
+++ Arrays/array.js
@@ -89,21 +89,3 @@ catch (e){
 }
 
 
-//Moving elements in an array
-const numbers = [1,2,3,4];
-
-const output = move(numbers, 5, 2);
-
-function move (array, index, offset){ 
-  const poistion = index + offset;
-  if (poistion >= array.length || poistion < 0){
-    console.error('Invalid offset');
-    return
-  }
-  const output = [...array]
-  const element = output.splice(index,1)[0];
-  output.splice(poistion,0,element);
-  return output;
-}
-
-console.log(output)
