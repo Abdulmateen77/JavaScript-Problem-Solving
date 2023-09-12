9a4dd56f5adb9672c03a5e10bd20e6c76e0a0862
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-12 23:59:57 +0100

    Update Moving-elements-in-an-array.js

--- Arrays/Moving-elements-in-an-array.js
+++ Arrays/Moving-elements-in-an-array.js
@@ -30,3 +30,27 @@ function move(array, index, offset) {
 
 // Display the result of the "move" function
 console.log(output);
+// Test Case 1: Move an element from index 2 to index 4 in the array
+const numbers1 = [1, 2, 3, 4];
+const output1 = move(numbers1, 2, 2);
+console.log(output1); // Should output [1, 2, 4, 3]
+
+// Test Case 2: Move an element from index 0 to index 3 in the array
+const numbers2 = [10, 20, 30, 40];
+const output2 = move(numbers2, 0, 3);
+console.log(output2); // Should output [20, 30, 40, 10]
+
+// Test Case 3: Try to move an element to an invalid negative index
+const numbers3 = [5, 6, 7, 8];
+const output3 = move(numbers3, 1, -2);
+console.log(output3); // Should display 'Invalid offset' and return undefined
+
+// Test Case 4: Try to move an element to an invalid index beyond the array length
+const numbers4 = [100, 200, 300];
+const output4 = move(numbers4, 2, 2);
+console.log(output4); // Should display 'Invalid offset' and return undefined
+
+// Test Case 5: Move an element within an array with a single element
+const numbers5 = [42];
+const output5 = move(numbers5, 0, 0);
+console.log(output5); // Should output [42]
