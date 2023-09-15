48439767c37264301156fca50dd34d3c2f43b5b9
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-15 23:19:32 +0100

    Update array.js

--- Arrays/array.js
+++ Arrays/array.js
@@ -39,25 +39,6 @@ function countOccurence(array, searchElement){
 
 console.log(countOccurence(number, 2))
 
-
-// Function to get max value of an array
-function getMax(array){
-  if (array.length === 0) return undefined;
-  //let max = array[0];
-  //for(let element = 1; element < array.length; element++)
-    //if (array[element] > max)
-      //max = array[element];
-  
-  //return max;
-
-  //With reduce method
-  return array.reduce ((a,b) => (a>b) ? a :b);
-}
-
-console.log(getMax([1,2,77,4,5]))
-
-
-
 // Sum of an array function
 function sumOfArugments(...items){
   if(items.length == 1 && Array.isArray(items[0]))
