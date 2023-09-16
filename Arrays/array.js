1d639fdee404fa2945abda70f594b31fb1a8639f
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-16 19:13:04 +0100

    Update array.js

--- Arrays/array.js
+++ Arrays/array.js
@@ -20,24 +20,6 @@ console.log(includes( numbers , 1));
 
 
 
-// Function to check the occurence of a number in an array
-const number = [1,2,3,2]
-
-function countOccurence(array, searchElement){
-  //let element = 0;
-  //for (let item of array)
-    //if(item === searchElement)
-      //element++;
-  //return element;
-
-  //with reduce function
-  return array.reduce((accumulator, current) => {
-    const occurence = (current === searchElement) ? 1 : 0
-    return accumulator + occurence;
-  }, 0);
-}
-
-console.log(countOccurence(number, 2))
 
 // Sum of an array function
 function sumOfArugments(...items){
