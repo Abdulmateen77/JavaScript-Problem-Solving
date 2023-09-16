21a2638891ca960071d7869148ca48fce1a1fa69
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-16 19:14:44 +0100

    Update check-the-occurence-of-a-number-in-an-array.js

--- Arrays/check-the-occurence-of-a-number-in-an-array.js
+++ Arrays/check-the-occurence-of-a-number-in-an-array.js
@@ -1,17 +1,31 @@
-const number = [1,2,3,2]
+// Define an array of numbers
+const numbers = [1, 2, 3, 2];
 
-function countOccurence(array, searchElement){
-  //let element = 0;
-  //for (let item of array)
-    //if(item === searchElement)
-      //element++;
-  //return element;
+// Function to count the occurrences of a search element in an array
+function countOccurrence(array, searchElement) {
+  // Uncommented code below is an alternative way to count occurrences using a loop
+  // Initialize a variable to count the occurrences
+  // let element = 0;
+  
+  // Iterate through the array and count occurrences of the search element
+  // for (let item of array) {
+  //   if (item === searchElement) {
+  //     element++;
+  //   }
+  // }
+  
+  // Return the count of occurrences
+  // return element;
 
-  //with reduce function
+  // With the reduce function, count occurrences of the search element in the array
   return array.reduce((accumulator, current) => {
-    const occurence = (current === searchElement) ? 1 : 0
-    return accumulator + occurence;
+    // Check if the current element matches the search element and assign 1 or 0 accordingly
+    const occurrence = (current === searchElement) ? 1 : 0;
+    
+    // Add the occurrence to the accumulator
+    return accumulator + occurrence;
   }, 0);
 }
 
-console.log(countOccurence(number, 2))
+// Call the countOccurrence function with the example array and search element and print the result
+console.log(countOccurrence(numbers, 2)); // Expected output: 2
