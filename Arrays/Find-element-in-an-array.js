9db02ff8735943ce77f192c7ebee4d042a72e91f
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-21 23:38:34 +0100

    Update Find-element-in-an-array.js

--- Arrays/Find-element-in-an-array.js
+++ Arrays/Find-element-in-an-array.js
@@ -15,5 +15,24 @@ function includes(array, searchElement) {
 // Example usage: an array of numbers
 const numbers = [1, 2, 3];
 
-// Call the includes function to check if 1 exists in the array
-console.log(includes(numbers, 1)); // Expected output: true
+const numbers = [1, 2, 3];
+const emptyArray = [];
+const strings = ["apple", "banana", "cherry"];
+const mixedTypes = [1, "apple", true, null, 42];
+
+console.log(includes(numbers, 1)); // Expected output: true (1 exists in the array)
+console.log(includes(numbers, 4)); // Expected output: false (4 does not exist in the array)
+
+console.log(includes(emptyArray, 1)); // Expected output: false (Empty array contains no elements)
+console.log(includes(emptyArray, undefined)); // Expected output: false (Empty array contains no elements)
+
+console.log(includes(strings, "banana")); // Expected output: true ("banana" exists in the array)
+console.log(includes(strings, "grape")); // Expected output: false ("grape" does not exist in the array)
+
+console.log(includes(mixedTypes, null)); // Expected output: true (null exists in the array)
+console.log(includes(mixedTypes, "apple")); // Expected output: true ("apple" exists in the array)
+console.log(includes(mixedTypes, false)); // Expected output: true (false exists in the array)
+console.log(includes(mixedTypes, 42)); // Expected output: true (42 exists in the array)
+console.log(includes(mixedTypes, "orange")); // Expected output: false ("orange" does not exist in the array)
+
+
