682ee3d2259c2ae60832e2ad1ec2778c3c60e696
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-23 22:11:18 +0100

    Update Sum-of-an-array-function.py

--- Arrays/Sum-of-an-array-function.py
+++ Arrays/Sum-of-an-array-function.py
@@ -1,8 +1,13 @@
-function sumOfArugments(...items){
-  if(items.length == 1 && Array.isArray(items[0]))
-    items = [...items[0]]
-  return items.reduce((a,b) => a+b);
-  
+// Define a function named sumOfArguments that accepts a variable number of arguments using the rest parameter (...items)
+function sumOfArguments(...items) {
+  // Check if there is only one argument and it is an array
+  if (items.length == 1 && Array.isArray(items[0])) {
+    // If the single argument is an array, spread its elements into the 'items' array
+    items = [...items[0]];
+  }
+  // Use the reduce method to calculate the sum of all elements in the 'items' array
+  return items.reduce((a, b) => a + b);
 }
 
-console.log(sumOfArugments([1,2,3,4]))
+// Example usage: Call the sumOfArguments function with an array as an argument
+console.log(sumOfArguments([1, 2, 3, 4])); // Expected output: 10 (sum of elements in the array)
