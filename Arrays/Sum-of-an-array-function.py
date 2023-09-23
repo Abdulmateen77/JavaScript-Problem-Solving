892298c05975a38bc08c655f01ac4fb04d44970d
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-23 22:12:08 +0100

    Update Sum-of-an-array-function.py

--- Arrays/Sum-of-an-array-function.py
+++ Arrays/Sum-of-an-array-function.py
@@ -11,3 +11,21 @@ function sumOfArguments(...items) {
 
 // Example usage: Call the sumOfArguments function with an array as an argument
 console.log(sumOfArguments([1, 2, 3, 4])); // Expected output: 10 (sum of elements in the array)
+
+
+console.log(sumOfArguments(1, 2, 3, 4)); // Expected output: 10 (sum of individual arguments)
+console.log(sumOfArguments([1, 2, 3, 4])); // Expected output: 10 (sum of elements in the array argument)
+
+// Test case with multiple arguments, including arrays
+console.log(sumOfArguments(1, [2, 3], 4)); // Expected output: 10 (sum of elements in individual arguments and array)
+
+console.log(sumOfArguments([1])); // Expected output: 1 (only one element in the array)
+
+console.log(sumOfArguments()); // Expected output: 0 (no arguments provided)
+
+console.log(sumOfArguments([1, 2], [3, 4])); // Expected output: 10 (sum of elements in multiple arrays)
+
+// Test case with negative numbers
+console.log(sumOfArguments(-1, -2, -3)); // Expected output: -6 (sum of negative numbers)
+
+console.log(sumOfArguments([-1, 2], [-3, 4])); // Expected output: 2 (sum of elements in multiple arrays with negative numbers)
