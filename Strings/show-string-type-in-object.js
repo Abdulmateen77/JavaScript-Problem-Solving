8d6b453b224bc1d6ba19e218ef819442ac899278
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-01 20:46:13 +0100

    Update show-string-type-in-object.js

--- Strings/show-string-type-in-object.js
+++ Strings/show-string-type-in-object.js
@@ -19,3 +19,47 @@ const movie = {
 
 // Call the showProperties function with the 'movie' object
 console.log(showProperties(movie));
+
+
+// Define an object with various data types
+const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York',
+  job: 'Engineer',
+  email: 'john@example.com'
+};
+
+// Test case 1: Display properties with string values in the 'person' object
+console.log("Test Case 1:");
+showProperties(person);
+
+// Define an object with all number values
+const numbers = {
+  one: 1,
+  two: 2,
+  three: 3
+};
+
+// Test case 2: Display properties with string values in the 'numbers' object
+console.log("\nTest Case 2:");
+showProperties(numbers);
+
+// Define an empty object
+const emptyObject = {};
+
+// Test case 3: Display properties with string values in an empty object
+console.log("\nTest Case 3:");
+showProperties(emptyObject);
+
+// Define an object with boolean and string values
+const mixedData = {
+  name: 'Alice',
+  age: 25,
+  isActive: true,
+  email: 'alice@example.com'
+};
+
+// Test case 4: Display properties with string values in the 'mixedData' object
+console.log("\nTest Case 4:");
+showProperties(mixedData);
