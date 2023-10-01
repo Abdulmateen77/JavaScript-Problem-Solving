ff78303e57c67a1bd12e2a5ae84743ecf5f07d61
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-01 20:45:40 +0100

    Update show-string-type-in-object.js

--- Strings/show-string-type-in-object.js
+++ Strings/show-string-type-in-object.js
@@ -1,13 +1,21 @@
-// function to show string type in object
-function showProperties(obj){
-  for (let prop in obj)
-    if (typeof obj[prop] === 'string') 
-      console.log (prop, obj[prop])
+// Function to display properties of an object that have string values
+function showProperties(obj) {
+  // Iterate over each property in the object
+  for (let prop in obj) {
+    // Check if the type of the property's value is 'string'
+    if (typeof obj[prop] === 'string') {
+      // If it's a string, log the property name and its value
+      console.log(prop, obj[prop]);
+    }
+  }
 }
 
- const movie = {
-   title: 'a',
-   year: 2022,
-   director: "pragy"
- };
- console.log(showProperties(movie))
+// Define an object called 'movie'
+const movie = {
+  title: 'a',
+  year: 2022,
+  director: "pragy"
+};
+
+// Call the showProperties function with the 'movie' object
+console.log(showProperties(movie));
