f51727312bac61ab577e7455696e9468f6637a8e
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-02 23:31:31 +0100

    Update objects.js

--- Objects/objects.js
+++ Objects/objects.js
@@ -24,21 +24,6 @@ function createAddress (street, city, zipCode){
 let addres = createAddress("oxford street","london", "W1C 1DL");
 console.log(addres)
 
-
-// function to check properties of two object
-function areEqual(address1, address2){
-  return address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-    
-}
-// function to check if the objects are same
-function areSame (address1, address2){
-   return address1 === address2;
-}
-console.log(areEqual(newAddress, newAddress2))
-console.log(areSame(newAddress, newAddress2))
-
 //Object to create blog post
 let blogPost = {
   title: "A",
