c916e1501256587cea1f20c04f2f1edaa89cb06c
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-02 23:32:20 +0100

    Create function-to-check-properties-of-two-object.js

---
+++ Objects/function-to-check-properties-of-two-object.js
@@ -0,0 +1,13 @@
+// function to check properties of two object
+function areEqual(address1, address2){
+  return address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+    
+}
+// function to check if the objects are same
+function areSame (address1, address2){
+   return address1 === address2;
+}
+console.log(areEqual(newAddress, newAddress2))
+console.log(areSame(newAddress, newAddress2))
