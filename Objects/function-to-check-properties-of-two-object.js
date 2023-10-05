91506e31b17a57dec77b13d2005acbb74973e37d
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-05 21:54:04 +0100

    Update function-to-check-properties-of-two-object.js

--- Objects/function-to-check-properties-of-two-object.js
+++ Objects/function-to-check-properties-of-two-object.js
@@ -14,18 +14,56 @@ function areSame(address1, address2) {
   return address1 === address2;
 }
 
-// Create two address objects for testing
-var newAddress = {
+// Test case 1: Objects with equal properties
+var address1 = {
   street: '123 Main St',
   city: 'Cityville',
   zipCode: '12345'
 };
 
-var newAddress2 = {
+var address2 = {
   street: '123 Main St',
   city: 'Cityville',
   zipCode: '12345'
 };
+
+console.log(areEqual(address1, address2)); // Output: true
+console.log(areSame(address1, address2));  // Output: false (They are different objects)
+
+// Test case 2: Objects with different street
+var address3 = {
+  street: '456 Elm St',
+  city: 'Townsville',
+  zipCode: '54321'
+};
+
+console.log(areEqual(address1, address3)); // Output: false (Street is different)
+console.log(areSame(address1, address3));  // Output: false (They are different objects)
+
+// Test case 3: Objects with different city
+var address4 = {
+  street: '123 Main St',
+  city: 'Villageton',
+  zipCode: '12345'
+};
+
+console.log(areEqual(address1, address4)); // Output: false (City is different)
+console.log(areSame(address1, address4));  // Output: false (They are different objects)
+
+// Test case 4: Objects with different zipCode
+var address5 = {
+  street: '123 Main St',
+  city: 'Cityville',
+  zipCode: '99999'
+};
+
+console.log(areEqual(address1, address5)); // Output: false (Zip code is different)
+console.log(areSame(address1, address5));  // Output: false (They are different objects)
+
+// Test case 5: Empty objects
+var emptyAddress1 = {};
+var emptyAddress2 = {};
+
 
 // Call the areEqual function to check if the properties of newAddress and newAddress2 are equal
 console.log(areEqual(newAddress, newAddress2)); // Output: true
