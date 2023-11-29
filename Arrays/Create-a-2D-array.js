aa7e103d3dd3f1f433e619a769804be3bd2ae585
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-11-29 22:53:17 +0000

    Update Create-a-2D-array.js

--- Arrays/Create-a-2D-array.js
+++ Arrays/Create-a-2D-array.js
@@ -23,20 +23,20 @@ if (JSON.stringify(createMatrix(2)) !== JSON.stringify([[0, 1], [0, 1]])) {
   console.log("Sample test case for input size = 2 passed!");
 }
 
-// Export the createMatrix function to be used in other modules
+//Export the createMatrix function to be used in other modules
 module.exports = createMatrix;
 
 
 const createMatrix = require('./createMatrix'); // Assuming 'createMatrix.js' is the file where the function is defined
 
-// Test case 1: Size = 1
+//Test case 1: Size = 1
 if (JSON.stringify(createMatrix(1)) !== JSON.stringify([[0]])) {
   console.log("Test fails: Expected [ [ 0 ] ] for input size = 1");
 } else {
   console.log("Test case for input size = 1 passed!");
 }
 
-// Test case 2: Size = 3
+//Test case 2: Size = 3
 if (
   JSON.stringify(createMatrix(3)) !==
   JSON.stringify([[0, 1, 2], [0, 1, 2], [0, 1, 2]])
@@ -46,7 +46,7 @@ if (
   console.log("Test case for input size = 3 passed!");
 }
 
-// Test case 3: Size = 4
+//Test case 3: Size = 4
 if (
   JSON.stringify(createMatrix(4)) !==
   JSON.stringify([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]])
@@ -56,4 +56,4 @@ if (
   console.log("Test case for input size = 4 passed!");
 }
 
-// Additional test cases can be added here...
+//Additional test cases can be added here...
