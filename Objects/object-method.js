ee61a2d20c964943ef6aab8a0d06c7b116ee928d
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-15 23:40:57 +0000

    Update object-method.js

--- Objects/object-method.js
+++ Objects/object-method.js
@@ -1,20 +1,31 @@
-
+// Define a function named methodOnObject with parameters name, age, college, city, and country.
 function methodOnObject(name, age, college, city, country) {
   // You need to implement this function.
-  let person=   {
+
+  // Create a person object with properties name, age, college, city, and country.
+  let person = {
     name: name,
     age: age,
     college: college,
     city: city,
     country: country,
 
+    // Define a method named getCity within the person object.
     getCity: function() {
-    return this.name + " lives in " + this.city + ".";
+      // Return a string that includes the person's name and the city they live in.
+      return this.name + " lives in " + this.city + ".";
     }
-  }
+  };
+
+  // Return the created person object.
   return person;
 }
-if ((methodOnObject("Abhinav", 22, "IIT Patna", "Patna", "India").getCity() === "Abhinav lives in Patna."))
+
+// Perform a sample test case.
+if (methodOnObject("Abhinav", 22, "IIT Patna", "Patna", "India").getCity() === "Abhinav lives in Patna.") {
+  // Log a success message if the test case passes.
   console.log("Sample test case 1 passed!");
-else
+} else {
+  // Log a failure message if the test case fails.
   console.log("Sample test case 1 failed!");
+}
