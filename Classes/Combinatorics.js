2fb7d9f02d9d18720db435443d4eef29143a06cf
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-18 23:12:15 +0000

    Update Combinatorics.js

--- Classes/Combinatorics.js
+++ Classes/Combinatorics.js
@@ -1,22 +1,35 @@
+// Define a class named Combinatorics
 class Combinatorics {
+
+      // Method to calculate the factorial of a number
       factorial(num){
+            // Base case: factorial of 0 and 1 is 1
             if(num === 0 || num === 1){
                   return 1;
             }
+            // Recursive case: Calculate factorial using recursion
             else{
                   return num * this.factorial(num-1);
             }        
       }
-      findCombinations(N,R){
+
+      // Method to find combinations (N choose R)
+      findCombinations(N, R){
+            // Check if R is greater than N, which is an invalid input
             if (R > N){
                   return 0;
             }
-            return this.factorial(N)/(this.factorial(R) * this.factorial(N-R));
+            // Calculate combinations using the factorial formula
+            return this.factorial(N) / (this.factorial(R) * this.factorial(N - R));
       }
-      findPermutations(N,R){
+
+      // Method to find permutations (N choose R)
+      findPermutations(N, R){
+            // Check if R is greater than N, which is an invalid input
             if (R > N){
                   return 0;
             }
-            return this.factorial(N)/ this.factorial(N-R);
+            // Calculate permutations using the factorial formula
+            return this.factorial(N) / this.factorial(N - R);
       }
 }
