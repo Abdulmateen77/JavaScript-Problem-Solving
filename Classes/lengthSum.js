0300a89f84c3699a51986d32025e04e74f38978b
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-18 23:12:49 +0000

    Update lengthSum.js

--- Classes/lengthSum.js
+++ Classes/lengthSum.js
@@ -1,32 +1,42 @@
+// Define a class named Length
 class Length {
-      constructor(feet,inch){
+      // Constructor to initialize the feet and inch properties
+      constructor(feet, inch){
             this.feet = feet;
             this.inch = inch;
       }
 
-      isValidObject(lengthObj){
+      // Method to check if the current Length object is valid
+      isValidObject(){
+            // Check if both feet and inches are non-negative
             if(this.feet >= 0 && this.inch >= 0){
-                  return true;
+                  return true; // The object is valid
             }
             else{
-                  return false;
+                  return false; // The object is not valid
             }
       }
 
+      // Method to add lengths of two Length objects
       addLength(lengthObj){
+            // Check if both the current object and the passed object are valid
             if (this.isValidObject() && lengthObj.isValidObject()){
+                  // Calculate total feet and total inches
                   let totalFeet = this.feet + lengthObj.feet;
                   let totalInches = this.inch + lengthObj.inch;
 
-            if (totalInches >= 12){
-                  totalFeet += Math.floor(totalInches / 12);
-                  totalInches %= 12;
-            }
-            return [totalFeet, totalInches];
+                  // Adjust total inches if it exceeds 12
+                  if (totalInches >= 12){
+                        totalFeet += Math.floor(totalInches / 12);
+                        totalInches %= 12;
+                  }
+
+                  // Return the result as an array [totalFeet, totalInches]
+                  return [totalFeet, totalInches];
             }
             else{
-                  return [0,0]
+                  // Return [0,0] if either or both objects are invalid
+                  return [0,0];
             }
-
       }
 }
