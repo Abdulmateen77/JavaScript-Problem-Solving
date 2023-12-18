181fb03c84edc351b231aa222d5f258d5d26b446
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-18 23:13:32 +0000

    Update wordKit.js

--- Classes/wordKit.js
+++ Classes/wordKit.js
@@ -1,26 +1,37 @@
+// Define a class named WordKit
 class WordKit {
+      // Method to reverse a given string
       reverse(str){
             let revStr = "";
             let chr;
 
-            for(let i =0; i < str.length; i++ ){
+            // Iterate through each character in the string
+            for(let i = 0; i < str.length; i++){
                   chr = str.charAt(i);
+                  // Add the character to the beginning of the reversed string
                   revStr = chr + revStr;
             }
 
+            // Return the reversed string
             return revStr;
       }
+
+      // Method to check if a string is a palindrome
       isPalindrome(str){
+            // Compare the original string with its reversed version
             if (str === this.reverse(str)){
-                  return true;
-            }else{
-                  return false;
+                  return true; // The string is a palindrome
+            } else {
+                  return false; // The string is not a palindrome
             }
       }
+
+      // Method to make a string a palindrome by appending its reversed version
       makePalindrome(str){
-            let newStr = this.reverse(str)
+            // Reverse the string
+            let newStr = this.reverse(str);
 
+            // Append the reversed string to the original string
             return str + newStr;
-
       }
 }
