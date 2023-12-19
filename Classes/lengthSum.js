082efc1d06e70a0965a22712b0166de55964aeb1
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-19 23:31:46 +0000

    Update lengthSum.js

--- Classes/lengthSum.js
+++ Classes/lengthSum.js
@@ -19,13 +19,13 @@ class Length {
 
       //Method to add lengths of two Length objects
       addLength(lengthObj){
-            // Check if both the current object and the passed object are valid
+            //Check if both the current object and the passed object are valid
             if (this.isValidObject() && lengthObj.isValidObject()){
-                  // Calculate total feet and total inches
+                  //Calculate total feet and total inches
                   let totalFeet = this.feet + lengthObj.feet;
                   let totalInches = this.inch + lengthObj.inch;
 
-                  // Adjust total inches if it exceeds 12
+                  //Adjust total inches if it exceeds 12
                   if (totalInches >= 12){
                         totalFeet += Math.floor(totalInches / 12);
                         totalInches %= 12;
