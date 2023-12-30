ba9d8e525d24c08b66d5cb560f9fc483ff9c9018
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-30 23:59:25 +0000

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -3,7 +3,7 @@ function Stopwatch() {
   //Private variables to track the state and duration of the stopwatch
   let startTime, endTime, running, duration = 0;
 
-  // Method to start the stopwatch
+  //Method to start the stopwatch
   this.start = function() {
     if (running)
       throw new Error('Stopwatch has already started.');
