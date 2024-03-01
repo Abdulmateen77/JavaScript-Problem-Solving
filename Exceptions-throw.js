15a268c21ef0c5def9971c70552829d69696361c
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-01 22:25:01 +0000

    Update Exceptions-throw.js

--- Exceptions-throw.js
+++ Exceptions-throw.js
@@ -33,6 +33,7 @@ console.log(isAlphabet('5')); //Output: Not Alphabet (non-alphabet character)
 console.log(isAlphabet('@')); //Output: Not Alphabet (non-alphabet character)
 console.log(isAlphabet(''));  //Output: Not Alphabet (empty string)
 
+
 //Test cases for checkAlphabet function
 console.log(checkAlphabet('A')); //Output: true (uppercase alphabet)
 console.log(checkAlphabet('z')); //Output: true (lowercase alphabet)
