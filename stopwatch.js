51f8a38af1902fd40fe3ec4e8307c42fa42cd722
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-09 23:52:53 +0000

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -10,12 +10,12 @@ function Stopwatch() {
       throw new Error('Stopwatch has already started.');
     running = true;
 
-    // Store the current time when the stopwatch starts
+    //Store the current time when the stopwatch starts
     
     startTime = new Date();
   }
 
-  // Method to stop the stopwatch
+  //Method to stop the stopwatch
   this.stop = function() {
     if (!running)
       throw new Error('Start the stopwatch');
