ab337d1cd25150cf3591485fb09686d26f1818db
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-13 22:34:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ function sum(limit){
 console.log(sum(10))
 //Function to show prime numbers up to a given number 'num'
 function showPrime(num) {
-  // Iterate through numbers from 2 to 'num'
+  //Iterate through numbers from 2 to 'num'
   for (let number = 2; number <= num; number++) {
-    // Check if the current number is prime using the 'isPrime' function
+    //Check if the current number is prime using the 'isPrime' function
     if (isPrime(number)) {
-      // If it's prime, print the prime number to the console
+      //If it's prime, print the prime number to the console
       console.log(number);
     }
   }
@@ -52,7 +52,7 @@ function showPrime(num) {
 function isPrime(number) {
   //Iterate through potential factors from 2 to 'number - 1'
   for (let factor = 2; factor < number; factor++) {
-    // If 'number' is divisible by 'factor' with no remainder, it's not prime
+    //If 'number' is divisible by 'factor' with no remainder, it's not prime
     if (number % factor === 0) {
       return false;
     }
