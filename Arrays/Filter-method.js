a13360b1b8a1fa636403270c7e7274d8323d56be
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-26 20:46:16 +0000

    Update Filter-method.js

--- Arrays/Filter-method.js
+++ Arrays/Filter-method.js
@@ -2,6 +2,7 @@
  * @param {number[]} ages - The array of ages to check for voting eligibility
  * @return {number[]} - The array of ages that are eligible to vote
  */
+
 function canVote(ages) {
   //Using the filter method to create a new array with ages >= 18
   const voterCheck = ages.filter((personAge) => personAge >= 18);
