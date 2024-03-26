ce26d9bf915b836a7a0a38f62874a6d666869a9c
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-26 20:46:52 +0000

    Update GetName-by-Map.js

--- Arrays/GetName-by-Map.js
+++ Arrays/GetName-by-Map.js
@@ -11,7 +11,9 @@ if (
     { firstname: "Dane", lastname: "Cobby" }
   ]).toString() != ["Robert Obrain", "Kevin Hart", "Dane Cobby"]
 ) {
+  
   console.log("Test fails: Expected output: ['Robert Obrain', 'Kevin Hart', 'Dane Cobby']");
+  
 } else {
   console.log("Sample test case passed!");
 }
