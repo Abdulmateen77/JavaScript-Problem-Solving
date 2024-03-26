162b0f6cdf0b47243d4ef05a69742042ed3645e0
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-03-26 20:46:02 +0000

    Update Filter-method.js

--- Arrays/Filter-method.js
+++ Arrays/Filter-method.js
@@ -3,14 +3,14 @@
  * @return {number[]} - The array of ages that are eligible to vote
  */
 function canVote(ages) {
-  // Using the filter method to create a new array with ages >= 18
+  //Using the filter method to create a new array with ages >= 18
   const voterCheck = ages.filter((personAge) => personAge >= 18);
 
-  // Returning the new array with ages that are eligible to vote
+  //Returning the new array with ages that are eligible to vote
   return voterCheck;
 }
 
-// Testing the function with a sample test case
+//Testing the function with a sample test case
 if (JSON.stringify(canVote([32, 33, 16, 40])) !== JSON.stringify([32, 33, 40])) {
   console.log("Test fails: Expected [32, 33, 40] for input ages = [32, 33, 16, 40]");
 } else {
