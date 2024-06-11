404c494ebbee1da3c1fdc27986cb32526a01777e
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-06-11 23:51:27 +0100

    Update constructor function-to-create-blog-posts.js

--- Objects/constructor function-to-create-blog-posts.js
+++ Objects/constructor function-to-create-blog-posts.js
@@ -9,7 +9,6 @@ function Post(title, body, author) {
   this.isLive = false; // Indicates if the post is live or not
 }
 
-
 // Create a new instance of the Post object
 let Blog = new Post("A", "B", "C");
 
